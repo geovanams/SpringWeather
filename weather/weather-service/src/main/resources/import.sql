7440d96633aa64e97cffc14a2134a64ad8a94bf0
geovanams <geeovanamartins@outlook.com>
2022-03-31 17:02:17 -0300

    Fisrt Commit

---
+++ weather/weather-service/src/main/resources/import.sql
@@ -0,0 +1,2 @@
+INSERT INTO weather (city, description, icon) VALUES ('Paris, France', 'Very cloudy!', 'weather-fog');
+INSERT INTO weather (city, description, icon) VALUES ('London, UK', 'Quite cloudy', 'weather-pouring');
